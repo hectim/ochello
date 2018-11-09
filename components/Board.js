f4408de4d5bf5330e31838f2e410dd63b249efe3
Brady Killeen <brady.killeen@tcnbroadcasting.com>
2018-11-09 09:29:21 -0700

    init

---
+++ components/Board.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>hi</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+})
